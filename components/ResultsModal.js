44f9b8db70a30a1bafc935c01431940a95ba8dc6
Kai Sackville-Hii <sackvillehiik@gmail.com>
2019-02-12 00:15:39 -0800

    In the proccess of adding custom Settings

--- components/ResultsModal.js
+++ components/ResultsModal.js
@@ -1,215 +1,328 @@
-import React from 'react';
-import { Text, View, StyleSheet, Dimensions} from 'react-native';
-import {futureValue, payment} from '../utils/pmtCalc';
-import { Button, Content, Col } from 'native-base';
-import { LineChart, Grid, YAxis, XAxis, PieChart} from 'react-native-svg-charts'
-import DynamicPieChart from '../components/DynamicPieChart';
-import Colors from '../constants/Colors';
+import React from "react";
+import { Text, View, StyleSheet, Dimensions } from "react-native";
+import { futureValue, payment } from "../utils/pmtCalc";
+import { Button, Content, Col } from "native-base";
+import {
+  LineChart,
+  Grid,
+  YAxis,
+  XAxis,
+  PieChart
+} from "react-native-svg-charts";
+import DynamicPieChart from "../components/DynamicPieChart";
+import Colors from "../constants/Colors";
+import { AsyncStorage } from "react-native";
 
-var data =[]
-const {width} = Dimensions.get('window')
-var numeral = require('numeral');
+var data = [];
+const { width } = Dimensions.get("window");
+var numeral = require("numeral");
 
 export class ResultsModal extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            calculating:true,
-            retirementIncome: null,
-            monthlySavings: null
-        }        
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      calculating: true,
+      retirementIncome: null,
+      monthlySavings: null
+    };
+  }
 
-    componentDidMount() {        
-        this.setState({calculating: true})
-        this.calculate();
-    }
+  // Desc: This function grabs the constants required for the calculations
+  //       from the local cache
+  // Post: If the keys in returnObj exist then the constant will be set with
+  //       this keys value, else a default value will be used.
+  _getCalcConstants = async () => {
+    try {
+      const returnObj = await AsyncStorage.multiGet([
+        "@constant:retirementAge",
+        "@constant:inflation",
+        "@constant:annualRates"
+      ]);
 
-    calculate(){ 
-        // // USER INPUT PROPS
-        var age = this.props.age;
-        var annualIncome = this.props.income;
+      // test if local cache is completely clear, if it is go straight to 
+      // default values.
+      if (returnObj !== []) {
+        let tempObj = {};
 
-        // ASSUMED CONSTANTS
-        const retireAge = 65;
-        const inflation = 0.035;
-        var annualRates = [0.04, 0.06, 0.08, 0.1];
-
-        // CALCULATED CONSTANTS
-        const yrsTillRet = retireAge - age;
-        const moTillRet = yrsTillRet * 12;
-
-        // retirement income or future value
-        var ri = futureValue(inflation, yrsTillRet, 0, annualIncome, 0);
-
-        var msArr = []
-        for (rate of annualRates) { 
-            var ms = payment(rate/12, moTillRet, 1, ri*10, 0) ;
-            msArr.push(ms.toFixed(2)) 
-        }
-        data = []
-        this.setState({retirementIncome: -1*ri.toFixed(2), monthlySavings: msArr, calculating: false})
-        for(let n of msArr){
-            data.push(Number(n))
+        // Check retirementAge
+        if (returnObj[0][1] == null) {
+          tempObj["retirementAge"] = 65;
+        } 
+        else {
+          tempObj["retirementAge"] = Number(returnObj[0][1]);
         }
 
+        // Check Inflation
+        if (returnObj[1][1] == null) {
+          tempObj["inflation"] = 0.035;
+        } 
+        else {
+          tempObj["inflation"] = Number(returnObj[1][1]);
+        }
+
+        // Check annual rates
+        if (returnObj[2][1] == null) {
+          tempObj["annualRates"] = [0.04, 0.06, 0.08, 0.1];
+        }
+        else {
+            // Key is an formated string with ¬ separator, separate on this will
+            // yeild an array, change each element to a number from string.
+            let listOfRates = returnObj[2][1].split("~");
+            for (let index in listOfRates) {
+                listOfRates[index] = Number(listOfRates[index]);
+            }
+
+          tempObj["annualRates"] = listOfRates;
+        }
+
+        return tempObj;
+      } 
+      else {
+        return {
+          retirementAge: 65,
+          inflation: 0.035,
+          annualRates: [0.04, 0.06, 0.08, 0.1]
+        };
+      }
+    } catch (error) {
+      console.log('catched at ResultsModal.js line 88. ERROR: ' + error);
     }
+  };
 
-    render() {
-        const contentInset = { top: 20, bottom: 20 };
-        const annualRates = [0.04, 0.06, 0.08, 0.1];
+  componentDidMount() {
+    this.setState({ calculating: true });
+    this.calculate();
+  }
 
-        return (
-            <View style={styles.container} collapsable={false}>
-                {
-                    this.state.calculating?
-                    (<Text>calculating...</Text>):
-                    (
-                        <View>
-                            <View>
-                                <View style={{height: 250,width: width, backgroundColor:Colors.primThree}}>
-                                    <Text style={{color:'white', fontSize: 18, fontWeight:'bold', alignSelf:'center', paddingTop: '5%'}}>Results</Text>
-                                    <View style={{height: '95%', width: '95%', flexDirection: 'row', paddingLeft:'4%',paddingRight:'4%',paddingBottom: '5%'}}>
-                                        <YAxis
-                                            data={ data }
-                                            style={{marginBottom:'5%'}}
-                                            contentInset={ contentInset }
-                                            svg={{
-                                                fill: 'white',
-                                                fontSize: 10,
-                                            }}
-                                            numberOfTicks={ 4 }
-                                            formatLabel={ value => numeral(value).format('0 a') }
-                                        />
-                                        <View style={{flex:1}}>
-                                            <LineChart
-                                                style={{ flex: 1, marginLeft: 16 }}
-                                                data={ data }
-                                                svg={{ stroke: 'white', strokeWidth: 4 }}
-                                                contentInset={{ top: 20, bottom: 20 }}
-                                            >
-                                                <Grid/>
-                                            </LineChart>
-                                            <XAxis
-                                                style={{ marginLeft: '5%', marginRight:'5%', marginBottom:'5%' }}
-                                                data={[4,6,8,10]}
-                                                formatLabel={ (value, index) => `${index}%` }
-                                                contentInset={{ left: 10, right: 10 }}
-                                                svg={{ fontSize: 10, fill: 'white' }}
-                                            />
-                                        </View>
-                                    </View>
-                                </View>
-                                <View style={{padding:'3%'}}>
-                                    
-                                    <View style={{flexDirection:'row'}}>
-                                        <View>
-                                            <Text style={styles.header}>Retirement Income</Text>
-                                            <Text style={styles.value}>{numeral(this.state.retirementIncome).format('$0,0.00')}</Text>
-                                        </View>
-                                        
-                                        <DynamicPieChart/>
+  // Desc: This function handles the logic of the calculator. it uses the constants from 
+  //    the local cache.
+  // Pre: must get the calculator constants before calculation can proceed.
+  // Post: State will be set with calculated values.
+  calculate = async () => {
+    let constObj = await this._getCalcConstants();
 
-                                    </View>
-                                    <View style={{marginTop:'8%'}}>
-                                        <Text style={styles.header}>Monthly Savings</Text>
-                                        <View style={styles.table}>
-                                            {
-                                                this.state.monthlySavings.map((save, index) =>
-                                                    <View style={styles.tableColumn} key={index}>
-                                                        <View style={styles.tableItem}>
-                                                            <Text style={styles.tableRate}>{`${annualRates[index]*10}%`}</Text>
-                                                        </View>
-                                                        <View style={styles.tableItem}>
-                                                            <Text style={styles.tableValue}>{numeral(save).format('$0,0.00')}</Text>
-                                                        </View>
-                                                    </View> 
-                                                )
-                                            }
-                                        </View>
-                                    </View>
-                            </View>
-                            <View style={styles.buttonContainer}>
-                                    <Button style={[styles.button, {backgroundColor:Colors.primThree}]} onPress={() => this.props._toggleSubview(false)}>
-                                        <Text style={styles.boldText}>DONE</Text>
-                                    </Button>
-                                    <Button style={[styles.button, {backgroundColor:Colors.primOne}]} onPress={() => this.props._share()}>
-                                        <Text style={styles.boldText}>SHARE</Text>
-                                    </Button>
-                            </View>
+    // // USER INPUT PROPS
+    var age = this.props.age;
+    var annualIncome = this.props.income;
+
+    // ASSUMED CONSTANTS
+    const retireAge = constObj.retirementAge;
+    const inflation = constObj.inflation;
+    var annualRates = constObj.annualRates;
+
+    // CALCULATED CONSTANTS
+    const yrsTillRet = retireAge - age;
+    const moTillRet = yrsTillRet * 12;
+
+    // retirement income or future value
+    var ri = futureValue(inflation, yrsTillRet, 0, annualIncome, 0);
+
+    var msArr = [];
+    for (rate of annualRates) {
+      var ms = payment(rate / 12, moTillRet, 1, ri * 10, 0);
+      msArr.push(ms.toFixed(2));
+    }
+    data = [];
+    this.setState({
+      retirementIncome: -1 * ri.toFixed(2),
+      monthlySavings: msArr,
+      calculating: false
+    });
+    for (let n of msArr) {
+      data.push(Number(n));
+    }
+  };
+
+  render() {
+    const contentInset = { top: 20, bottom: 20 };
+    const annualRates = [0.04, 0.06, 0.08, 0.1];
+
+    return (
+      <View style={styles.container} collapsable={false}>
+        {this.state.calculating ? (
+          <Text>calculating...</Text>
+        ) : (
+          <View>
+            <View>
+              <View
+                style={{
+                  height: 250,
+                  width: width,
+                  backgroundColor: Colors.primThree
+                }}
+              >
+                <Text
+                  style={{
+                    color: "white",
+                    fontSize: 18,
+                    fontWeight: "bold",
+                    alignSelf: "center",
+                    paddingTop: "5%"
+                  }}
+                >
+                  Results
+                </Text>
+                <View
+                  style={{
+                    height: "95%",
+                    width: "95%",
+                    flexDirection: "row",
+                    paddingLeft: "4%",
+                    paddingRight: "4%",
+                    paddingBottom: "5%"
+                  }}
+                >
+                  <YAxis
+                    data={data}
+                    style={{ marginBottom: "5%" }}
+                    contentInset={contentInset}
+                    svg={{
+                      fill: "white",
+                      fontSize: 10
+                    }}
+                    numberOfTicks={4}
+                    formatLabel={value => numeral(value).format("0 a")}
+                  />
+                  <View style={{ flex: 1 }}>
+                    <LineChart
+                      style={{ flex: 1, marginLeft: 16 }}
+                      data={data}
+                      svg={{ stroke: "white", strokeWidth: 4 }}
+                      contentInset={{ top: 20, bottom: 20 }}
+                    >
+                      <Grid />
+                    </LineChart>
+                    <XAxis
+                      style={{
+                        marginLeft: "5%",
+                        marginRight: "5%",
+                        marginBottom: "5%"
+                      }}
+                      data={[4, 6, 8, 10]}
+                      formatLabel={(value, index) => `${index}%`}
+                      contentInset={{ left: 10, right: 10 }}
+                      svg={{ fontSize: 10, fill: "white" }}
+                    />
+                  </View>
+                </View>
+              </View>
+              <View style={{ padding: "3%" }}>
+                <View style={{ flexDirection: "row" }}>
+                  <View>
+                    <Text style={styles.header}>Retirement Income</Text>
+                    <Text style={styles.value}>
+                      {numeral(this.state.retirementIncome).format("$0,0.00")}
+                    </Text>
+                  </View>
+
+                  <DynamicPieChart />
+                </View>
+                <View style={{ marginTop: "8%" }}>
+                  <Text style={styles.header}>Monthly Savings</Text>
+                  <View style={styles.table}>
+                    {this.state.monthlySavings.map((save, index) => (
+                      <View style={styles.tableColumn} key={index}>
+                        <View style={styles.tableItem}>
+                          <Text style={styles.tableRate}>{`${annualRates[
+                            index
+                          ] * 10}%`}</Text>
                         </View>
-                    </View>
-                    )
-                }
+                        <View style={styles.tableItem}>
+                          <Text style={styles.tableValue}>
+                            {numeral(save).format("$0,0.00")}
+                          </Text>
+                        </View>
+                      </View>
+                    ))}
+                  </View>
+                </View>
+              </View>
+              <View style={styles.buttonContainer}>
+                <Button
+                  style={[styles.button, { backgroundColor: Colors.primThree }]}
+                  onPress={() => this.props._toggleSubview(false)}
+                >
+                  <Text style={styles.boldText}>DONE</Text>
+                </Button>
+                <Button
+                  style={[styles.button, { backgroundColor: Colors.primOne }]}
+                  onPress={() => this.props._share()}
+                >
+                  <Text style={styles.boldText}>SHARE</Text>
+                </Button>
+              </View>
             </View>
-        )
-    }
+          </View>
+        )}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        // padding: 20,
-    },
-    boldText: {
-        fontWeight:'bold',
-        color: 'white'
-    },
-    header: {
-        fontSize: 18,
-        fontWeight: '700',
-        paddingTop: 5,
-        color: Colors.fgLight
-    },
-    value: {
-        padding: 5,
-        alignItems: 'center',
-        fontSize: 30,
-        color: Colors.primThree,
-        fontWeight: '700'
-    },
-    table: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        minHeight: 70,
-        paddingTop: '5%',
-    },
-    tableColumn: {
-        flex: 1,
-        flexDirection: 'column',
-        minWidth: 80,
-    },
-    tableItem: {
-        flex:1,
-        borderWidth: 1,
-        borderColor: Colors.fgLight,
-        padding:'10%'
-    },
-    tableValue: {
-        // padding: 5,
-        // alignItems: 'center',
-        // fontSize: 18,
-        color: Colors.primThree,
-        alignSelf:'center',
-        fontWeight: '700'
-    },
-    tableRate: {
-        // padding: 5,
-        // alignItems: 'center',
-        // fontSize: 18,
-        color: Colors.fgLight,
-        alignSelf:'center',
-        fontWeight: '700'
-    },
-    buttonContainer: {
-        flexDirection: 'row',
-        alignSelf: 'center',
-        paddingTop: '30%'
-      },
-    button: {
-        margin: 10,
-        width: 120,
-        height: 60,
-        justifyContent: "center",
-      },
-})
+  container: {
+    // padding: 20,
+  },
+  boldText: {
+    fontWeight: "bold",
+    color: "white"
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: "700",
+    paddingTop: 5,
+    color: Colors.fgLight
+  },
+  value: {
+    padding: 5,
+    alignItems: "center",
+    fontSize: 30,
+    color: Colors.primThree,
+    fontWeight: "700"
+  },
+  table: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    minHeight: 70,
+    paddingTop: "5%"
+  },
+  tableColumn: {
+    flex: 1,
+    flexDirection: "column",
+    minWidth: 80
+  },
+  tableItem: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: Colors.fgLight,
+    padding: "10%"
+  },
+  tableValue: {
+    // padding: 5,
+    // alignItems: 'center',
+    // fontSize: 18,
+    color: Colors.primThree,
+    alignSelf: "center",
+    fontWeight: "700"
+  },
+  tableRate: {
+    // padding: 5,
+    // alignItems: 'center',
+    // fontSize: 18,
+    color: Colors.fgLight,
+    alignSelf: "center",
+    fontWeight: "700"
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    alignSelf: "center",
+    paddingTop: "30%"
+  },
+  button: {
+    margin: 10,
+    width: 120,
+    height: 60,
+    justifyContent: "center"
+  }
+});
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,22 +1,250 @@
-import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
-import Colors from '../constants/Colors';
+import React from "react";
+import { SectionList, Image, StyleSheet, Text, View } from "react-native";
+import Colors from "../constants/Colors";
+import { AsyncStorage } from "react-native";
+import { Constants } from "expo";
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'Settings',
-    headerTitleStyle:{
-      color:'white',  
-      alignSelf:'center'
+    title: "Settings",
+    headerTitleStyle: {
+      color: "white",
+      alignSelf: "center"
     },
     headerStyle: {
-      backgroundColor: Colors.fgDark,
-    },
+      backgroundColor: Colors.fgDark
+    }
+  };
+
+  constructor() {
+    super();
+
+    this.state = {
+      retirementAge: 65,
+      inflationRate: "3.5 %",
+      annualRates: "[2, 4, 8, 10] %"
+    };
+  }
+
+  _setCalcConstants = async () => {
+    try {
+      await AsyncStorage.setItem("@constant:retirementAge", "65");
+      await AsyncStorage.setItem("@constant:annualRates", "0.04~0.06~0.08~0.1");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    const { manifest } = Constants;
+    const sections = [
+      {
+        data: [
+          {
+            value:
+              "The age at which most people normally retire from work, default age set to 65.",
+            constant: this.state.retirementAge
+          }
+        ],
+        title: "Retirement Age"
+      },
+      {
+        data: [
+          {
+            value:
+              "Inflation indicates a decrease in the purchasing power of a nation's currency, default value is 3.5%",
+            constant: this.state.inflationRate
+          }
+        ],
+        title: "Inflation"
+      },
+      {
+        data: [
+          {
+            value:
+              "Is the interest rate for a whole year, rather than just a monthly fee/rate, as applied on a loan, mortgage loan, credit card, etc. It is a finance charge expressed as an annual rate",
+              constant: this.state.annualRates
+          }
+        ],
+        title: "Annual Rates"
+      }
+    ];
+    return (
+      <SectionList
+        style={styles.container}
+        renderItem={this._renderItem}
+        renderSectionHeader={this._renderSectionHeader}
+        stickySectionHeadersEnabled={true}
+        keyExtractor={(item, index) => index}
+        ListHeaderComponent={ListHeader}
+        sections={sections}
+      />
+    );
   }
+
+  _renderSectionHeader = ({ section }) => {
+    return (
+    <SectionHeader title={section.title} />
+    );
+  };
+
+  _renderItem = ({ item }) => {
+    if (item.type === "color") {
+      return (
+        <SectionContent>
+          {item.value && <Color value={item.value} />}
+        </SectionContent>
+      );
+    } else {
+      return (
+        <SectionContent>
+          <Text
+            style={{
+              fontWeight: "bold",
+              marginTop: "1.5%",
+              marginBottom: "1.5%",
+              fontSize: 20,
+              alignSelf: "center"
+            }}
+          >
+            {item.constant}
+          </Text>
+          <Text style={styles.sectionContentText}>{item.value}</Text>
+        </SectionContent>
+      );
+    }
+  };
 }
+
+const ListHeader = () => {
+  const { manifest } = Constants;
+
+  return (
+    <View style={styles.titleContainer}>
+      <View style={styles.titleIconContainer}>
+        <AppIconPreview iconUrl={manifest.iconUrl} />
+      </View>
+
+      <View style={styles.titleTextContainer}>
+        <Text style={styles.nameText} numberOfLines={1}>
+          Financial Calculator TM
+        </Text>
+
+        <Text style={styles.slugText} numberOfLines={1}>
+          By KVN Software Solutions
+        </Text>
+
+        <Text style={styles.descriptionText}>Feel free to change the value of constants below</Text>
+        <Text>Edit</Text>
+      </View>    
+
+    </View>
+  );
+};
+
+const SectionHeader = ({ title }) => {
+  return (
+    <View style={styles.sectionHeaderContainer}>
+      <Text style={styles.sectionHeaderText}>{title}</Text>
+    </View>
+  );
+};
+
+const SectionContent = props => {
+  return <View style={styles.sectionContentContainer}>{props.children}</View>;
+};
+
+const AppIconPreview = ({ iconUrl }) => {
+  if (!iconUrl) {
+    iconUrl =
+      "https://s3.amazonaws.com/exp-brand-assets/ExponentEmptyManifest_192.png";
+  }
+
+  return (
+    <Image
+      source={require("../assets/images/icon.png")}
+      style={{ width: 64, height: 64 }}
+      resizeMode="cover"
+    />
+  );
+};
+
+const Color = ({ value }) => {
+  if (!value) {
+    return <View />;
+  } else {
+    return (
+      <View style={styles.colorContainer}>
+        <View style={[styles.colorPreview, { backgroundColor: value }]} />
+        <View style={styles.colorTextContainer}>
+          <Text style={styles.sectionContentText}>{value}</Text>
+        </View>
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  titleContainer: {
+    paddingHorizontal: 15,
+    paddingTop: 15,
+    paddingBottom: 15,
+    flexDirection: "row"
+  },
+  titleIconContainer: {
+    marginRight: 15,
+    paddingTop: 2
+  },
+  sectionHeaderContainer: {
+    backgroundColor: "#fbfbfb",
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "#ededed"
+  },
+  sectionHeaderText: {
+    fontSize: 14
+  },
+  sectionContentContainer: {
+    paddingTop: 8,
+    paddingBottom: 12,
+    paddingHorizontal: 15
+  },
+  sectionContentText: {
+    color: "#808080",
+    fontSize: 14
+  },
+  nameText: {
+    fontWeight: "600",
+    fontSize: 18
+  },
+  slugText: {
+    color: "#a39f9f",
+    fontSize: 14,
+    backgroundColor: "transparent"
+  },
+  descriptionText: {
+    fontSize: 14,
+    marginTop: 6,
+    color: "#4d4d4d"
+  },
+  colorContainer: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  colorPreview: {
+    width: 17,
+    height: 17,
+    borderRadius: 2,
+    marginRight: 6,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "#ccc"
+  },
+  colorTextContainer: {
+    flex: 1
+  }
+});
